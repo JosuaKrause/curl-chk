f46824b04e79886cea1e48f222cc9a37cf27a760
Josua Krause <josua.krause@gmail.com>
2015-05-25 21:38:50 -0400

    init

---
+++ readme.md
@@ -0,0 +1,25 @@
+# curl_chk – checksum wrapper for curl
+
+curl_chk is a wrapper for curl that verifies the content
+of the actual file that was downloaded via its md5 sum.
+
+[![Build Status](https://travis-ci.org/JosuaKrause/curl_chk.svg?branch=master)](https://travis-ci.org/JosuaKrause/curl_chk)
+
+Currently only one file at a time is allowed. The local
+name of this file has to be specified via `-o` and the
+md5 sum can either be passed as part of the URL (only
+if the URL was specified via `--url`) or explicitely as
+argument of curl `--md5`. All other arguments are used
+as is and as specified by curl.
+
+Example calls:
+```
+./curl --url "http://example.com/#md5=09b9c392dc1f6e914cea287cb6be34b0" -o "index.html"
+./curl --md5 09b9c392dc1f6e914cea287cb6be34b0 -o "index.html" "http://example.com/"
+```
+
+You can use the wrapper either directly or by renaming
+the original, changing `REAL_CURL` in the script, and
+moving the script into path.
+
+Pull requests are welcome!
---
+++ test/run.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+cd "$(dirname $0)"
+
+TEST_DIR="./out"
+STD_OUT="${TEST_DIR}/out.tmp"
+GITHUB_USER="JosuaKrause"
+GITHUB_REPO="curl_chk"
+GITHUB_BRANCH="master"
+GITHUB_PREFIX="https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${GITHUB_BRANCH}/test"
+
+echo "running tests.."
+echo "the github prefix is: ${GITHUB_PREFIX}"
+
+if [ ! -d "${TEST_DIR}" ]; then
+  mkdir -p "${TEST_DIR}"
+fi
+
+check_exit() {
+  if [ -f "${STD_OUT}" ]; then
+    cat "${STD_OUT}"
+  fi
+  if [ $1 != $2 ]; then
+    echo "^ unexpected exit status! expected $2 got $1 ^"
+    exit 1
+  fi
+}
+
+check_file() {
+  diff -q "$1" "$2"
+  if [ $? -ne 0 ]; then
+    diff -u "$1" "$2"
+    print "^ $2 doesn't match $1 ^"
+    exit 2
+  fi
+}
+
+md5_a="d8e8fca2dc0f896fd7cb4cb0031ba249"
+md5_b="95b3644556b48a25f3366d82b0e3b349"
+
+## using explicit --md5
+# correct verification
+echo "test 1"
+../curl -# -o "${TEST_DIR}/a.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/a.test" > "${STD_OUT}"
+check_exit $? 0
+check_file "a.test" "${TEST_DIR}/a.tmp"
+grep -q "md5: '${md5_a}'" "${STD_OUT}"
+x=$?
+rm -- "${STD_OUT}"
+check_exit $x 0
+
+# wrong md5 sum
+echo "test 2"
+../curl -# -o "${TEST_DIR}/b.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/b.test"
+check_exit $? 69
+check_file "b.test" "${TEST_DIR}/b.tmp"
+
+## using URL fragment
+# correct verification
+echo "test 3"
+../curl -# -o "${TEST_DIR}/b.tmp" --url "${GITHUB_PREFIX}/b.test#md5=${md5_b}" > "${STD_OUT}"
+check_exit $? 0
+check_file "b.test" "${TEST_DIR}/b.tmp"
+grep -q "md5: '${md5_b}'" "${STD_OUT}"
+x=$?
+rm -- "${STD_OUT}"
+check_exit $x 0
+
+# wrong md5 sum
+echo "test 4"
+../curl -# -o "${TEST_DIR}/a.tmp" --url "${GITHUB_PREFIX}/a.test#md5=${md5_b}"
+check_exit $? 69
+check_file "a.test" "${TEST_DIR}/a.tmp"
+
+echo "cleaning up"
+rm -r -- "${TEST_DIR}"
+exit 0
