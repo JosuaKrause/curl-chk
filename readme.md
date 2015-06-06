af1743420258a97ba4d4df5ddde0cffc59894c09
Josua Krause <josua.krause@gmail.com>
2015-06-06 12:21:44 -0400

    multiple files per curl and automatic filename inference fixes #1 fixes #2

--- readme.md
+++ readme.md
@@ -1,22 +1,31 @@
 # curl-chk – checksum wrapper for curl
 
 curl_chk is a wrapper for curl that verifies the content
-of the actual file that was downloaded via its md5 sum.
+of the actual files that were downloaded via their md5 sums.
 
 [![Build Status](https://travis-ci.org/JosuaKrause/curl-chk.svg?branch=master)](https://travis-ci.org/JosuaKrause/curl-chk)
 
-Currently only one file at a time is allowed. The local
-name of this file has to be specified via `-o` and the
-md5 sum can either be passed as part of the URL (only
-if the URL was specified via `--url`) or explicitely as
-argument of curl `--md5`. All other arguments are used
-as is and as specified by curl.
+The md5 sum can either be passed as part of the URL or explicitely as
+argument `--md5`. All other arguments are used as is and as specified by curl.
+The results of the integrity check are printed to *stderr* in the format:
+
+```
+./index.html: OK 09b9c392dc1f6e914cea287cb6be34b0
+./foo.txt: FAILED d8e8fca2dc0f896fd7cb4cb0031ba249
+WARNING: 1 of 2 computed checksums did NOT match
+```
+
+The warning line only appears if at least one of the checks failed.
 
 Example calls:
-```
-./curl --url "http://example.com/#md5=09b9c392dc1f6e914cea287cb6be34b0" -o "index.html"
+```bash
+./curl "http://example.com/#md5=09b9c392dc1f6e914cea287cb6be34b0" -o "index.html"
 ./curl --md5 09b9c392dc1f6e914cea287cb6be34b0 -o "index.html" "http://example.com/"
+./curl -O "http://example.com/index.html#md5=09b9c392dc1f6e914cea287cb6be34b0"
 ```
+
+Note that `-O` in the example saves the file as `index.html#md5=09b9c392dc1f6e914cea287cb6be34b0`.
+You can use `--md5` to avoid this.
 
 You can use the wrapper either directly or by renaming
 the original, changing `REAL_CURL` in the script, and
--- test/run.sh
+++ test/run.sh
@@ -3,22 +3,36 @@
 cd "$(dirname $0)"
 
 TEST_DIR="./out"
-STD_OUT="${TEST_DIR}/out.tmp"
+STD_ERR="${TEST_DIR}/err.tmp"
 GITHUB_USER="JosuaKrause"
 GITHUB_REPO="curl_chk"
 GITHUB_BRANCH="master"
 GITHUB_PREFIX="https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${GITHUB_BRANCH}/test"
 
-echo "running tests.."
-echo "the github prefix is: ${GITHUB_PREFIX}"
+echo "NOTICE: running tests.."
+echo "NOTICE: the github prefix is: ${GITHUB_PREFIX}"
 
 if [ ! -d "${TEST_DIR}" ]; then
   mkdir -p "${TEST_DIR}"
 fi
 
+run() {
+  local err="$1"; shift
+  local ret=1
+  echo "NOTICE: $@"
+  if [ "${err}" == "-" ]; then
+    "$@"
+    ret=$?
+  else
+    "$@" 2> "${err}"
+    ret=$?
+  fi
+  return $ret
+}
+
 check_exit() {
-  if [ -f "${STD_OUT}" ]; then
-    cat "${STD_OUT}"
+  if [ -f "${STD_ERR}" ]; then
+    cat "${STD_ERR}"
   fi
   if [ $1 != $2 ]; then
     echo "^ unexpected exit status! expected $2 got $1 ^"
@@ -30,15 +44,15 @@ check_file() {
   diff -q "$1" "$2"
   if [ $? -ne 0 ]; then
     diff -u "$1" "$2"
-    print "^ $2 doesn't match $1 ^"
+    echo "^ $2 doesn't match $1 ^"
     exit 2
   fi
 }
 
 num_tests=0
 begin_test() {
-  ((num_tests++))
-  echo "test ${num_tests}"
+  num_tests=$((num_tests+1))
+  echo "NOTICE: test ${num_tests}"
 }
 
 md5_a="d8e8fca2dc0f896fd7cb4cb0031ba249"
@@ -47,39 +61,68 @@ md5_b="95b3644556b48a25f3366d82b0e3b349"
 ## using explicit --md5
 # correct verification
 begin_test
-../curl -# -o "${TEST_DIR}/a.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/a.test" > "${STD_OUT}"
+run "${STD_ERR}" ../curl -# -o "${TEST_DIR}/a.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/a.test"
 check_exit $? 0
 check_file "a.test" "${TEST_DIR}/a.tmp"
-grep -q "md5: '${md5_a}'" "${STD_OUT}"
-x=$?
-rm -- "${STD_OUT}"
-check_exit $x 0
+run "-" cat "${STD_ERR}"
+run "-" grep -q "${TEST_DIR}/a.tmp: OK ${md5_a}" "${STD_ERR}"
+check_exit $? 0
+rm -- "${STD_ERR}"
 
 # wrong md5 sum
 begin_test
-../curl -# -o "${TEST_DIR}/b.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/b.test"
-check_exit $? 69
+run "${STD_ERR}" ../curl -# -o "${TEST_DIR}/b.tmp" --md5 "${md5_a}" "${GITHUB_PREFIX}/b.test"
+check_exit $? 42
 check_file "b.test" "${TEST_DIR}/b.tmp"
+run "-" cat "${STD_ERR}"
+run "-" grep -q "${TEST_DIR}/b.tmp: FAILED ${md5_a}" "${STD_ERR}"
+check_exit $? 0
+run "-" grep -q "WARNING: 1 of 1 computed checksums did NOT match" "${STD_ERR}"
+check_exit $? 0
+rm -- "${STD_ERR}"
 
 ## using URL fragment
 # correct verification
 begin_test
-../curl -# -o "${TEST_DIR}/b.tmp" --url "${GITHUB_PREFIX}/b.test#md5=${md5_b}" > "${STD_OUT}"
+run "${STD_ERR}" ../curl -# -o "${TEST_DIR}/b.tmp" --url "${GITHUB_PREFIX}/b.test#md5=${md5_b}"
 check_exit $? 0
 check_file "b.test" "${TEST_DIR}/b.tmp"
-grep -q "md5: '${md5_b}'" "${STD_OUT}"
-x=$?
-rm -- "${STD_OUT}"
-check_exit $x 0
+run "-" cat "${STD_ERR}"
+run "-" grep -q "${TEST_DIR}/b.tmp: OK ${md5_b}" "${STD_ERR}"
+check_exit $? 0
+rm -- "${STD_ERR}"
 
 # wrong md5 sum
 begin_test
-../curl -# -o "${TEST_DIR}/a.tmp" --url "${GITHUB_PREFIX}/a.test#md5=${md5_b}"
-check_exit $? 69
+run "${STD_ERR}" ../curl -# -o "${TEST_DIR}/a.tmp" --url "${GITHUB_PREFIX}/a.test#md5=${md5_b}"
+check_exit $? 42
 check_file "a.test" "${TEST_DIR}/a.tmp"
+run "-" cat "${STD_ERR}"
+run "-" grep -q "${TEST_DIR}/a.tmp: FAILED ${md5_b}" "${STD_ERR}"
+check_exit $? 0
+run "-" grep -q "WARNING: 1 of 1 computed checksums did NOT match" "${STD_ERR}"
+check_exit $? 0
+rm -- "${STD_ERR}"
 
-echo "cleaning up"
+## auto inference
+# correct verification - multiple files
+begin_test
+cd "${TEST_DIR}"
+run "../${STD_ERR}" ../../curl -# --remote-name-all "${GITHUB_PREFIX}/a.test#md5=${md5_a}" "${GITHUB_PREFIX}/b.test?md5=${md5_b}"
+x=$?
+cd ".."
+check_exit $x 0
+check_file "a.test" "${TEST_DIR}/a.test#md5=${md5_a}"
+check_file "b.test" "${TEST_DIR}/b.test?md5=${md5_b}"
+run "-" cat "${STD_ERR}"
+run "-" grep -q "a.test#md5=${md5_a}: OK ${md5_a}" "${STD_ERR}"
+check_exit $? 0
+run "-" grep -q "b.test?md5=${md5_b}: OK ${md5_b}" "${STD_ERR}"
+check_exit $? 0
+rm -- "${STD_ERR}"
+
+echo "NOTICE: cleaning up"
 rm -r -- "${TEST_DIR}"
 
-echo "all tests were successful!"
+echo "NOTICE: all tests were successful!"
 exit 0
